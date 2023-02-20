1cd1744d818b76b39a3a8596c1e90090e0168aa6
MatAdamko <mat.adamko@gmail.com>
2023-02-20 19:51:59 +0100

    ApplicationContext
    Added AplicationContext for database functionality

---
+++ Bootcamp.ToDoList/Bootcamp.ToDoList.Backend/Database/ApplicationContext.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bootcamp.ToDoList.Backend.Entities.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bootcamp.ToDoList.Backend.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        
+        public ApplicationContext(IConfiguration configuration, DbContextOptions<ApplicationContext> options)
+            : base(options)
+        { 
+            Configuration = configuration;
+        }
+        
+        public IConfiguration Configuration { get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString),
+                    opts =>
+                    {
+                        opts.MigrationsAssembly("Bootcamp.ToDoList.Backend");
+                    });
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
--- Bootcamp.ToDoList/Bootcamp.ToDoList.Backend/Services/ItemService.cs
+++ Bootcamp.ToDoList/Bootcamp.ToDoList.Backend/Services/ItemService.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bootcamp.ToDoList.Backend.Services.Interfaces;
 
 namespace Bootcamp.ToDoList.Backend.Services
 {
-    public class ItemService
+    public class ItemService : IItemService
     {
         
     }
