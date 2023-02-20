8a32db3d08287b5808c4325d4d319b3e1cbe5dbd
MatAdamko <mat.adamko@gmail.com>
2023-02-20 13:51:02 +0100

    Initial commit
    Initial commit

---
+++ Bootcamp.ToDoList/Bootcamp.ToDoList.Backend/Entities/DTO/Itme/ItemDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootcamp.ToDoList.Backend.Entities.DTO
+{
+    public class ItemDto
+    {
+        
+    }
+}
