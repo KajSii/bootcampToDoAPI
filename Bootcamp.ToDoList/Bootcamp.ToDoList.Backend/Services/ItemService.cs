56300e97de529530a04fac219df5c469679ff0d0
Tibor Krátky <124772440+KajSii@users.noreply.github.com>
2023-02-21 11:49:59 +0100

    Revert "BE-CreateItem"

--- Bootcamp.ToDoList/Bootcamp.ToDoList.Backend/Services/ItemService.cs
+++ Bootcamp.ToDoList/Bootcamp.ToDoList.Backend/Services/ItemService.cs
@@ -1,81 +1,36 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Bootcamp.ToDoList.Backend.Entities.DTO;
 using Bootcamp.ToDoList.Backend.Entities.Models;
 using Bootcamp.ToDoList.Backend.Services.Interfaces;
-using Bootcamp.ToDoList.Backend.Database;
-using Bootcamp.ToDoList.Backend.Exceptions;
-using Microsoft.EntityFrameworkCore;
-using Bootcamp.ToDoList.Backend.Entities.Domain;
-using System.Linq.Expressions;
 
 namespace Bootcamp.ToDoList.Backend.Services
 {
     public class ItemService : IItemService
     {
-        private ApplicationContext _context;
-
-        public ItemService(ApplicationContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<ItemDto> CreateItemAsync(ItemModel model, CancellationToken ct = default)
-        {
-            if (await _context.Items.AnyAsync(x => x.Name == model.Name, ct))
-            {
-                throw new ConflictException($"Item with name {model.Name} already exists");
-            }
-
-            var item = model.ToDomain();
-            item.PublicId = Guid.NewGuid();
-
-            await _context.Items.AddAsync(item, ct);
-            await _context.SaveChangesAsync(ct);
-
-            return item.ToDto();
-        }
-
-        public Task DeleteItemAsync(Guid itemId, CancellationToken ct = default)
+        Task<ItemDto> IItemService.CreateItemAsync(ItemModel model, CancellationToken ct)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<ItemDto> GetItemAsync(Guid itemId, CancellationToken ct = default)
+        Task IItemService.DeleteItemAsync(Guid itemId, CancellationToken ct)
         {
-            var item = await _context.Items
-                .AsNoTracking()
-                .SingleOrDefaultAsync(x => x.PublicId == itemId, ct);
-
-            if (item == null)
-            {
-                throw new NotFoundException($"Item with Id: {itemId} doesn't exist.");
-            }
-
-            return item.ToDto();
+            throw new NotImplementedException();
         }
 
-        public async Task<List<ItemDto>> GetAllItemsAsync(int? pageSize = null, CancellationToken ct = default)
+        Task<ItemDto> IItemService.GetItemAsync(Guid itemId, CancellationToken ct)
         {
-            IQueryable<Item> query = _context.Items
-                .AsNoTracking()
-                .AsQueryable();
-
-            if (pageSize > 0)
-            {
-                query = query.Take(pageSize.Value);
-            }
-
-            List<Item> items = await query.ToListAsync();
-            List<ItemDto> dtos = items.Select(x => x.ToDto()).ToList();
-
-            return dtos;
+            throw new NotImplementedException();
         }
 
-        public Task<ItemDto> UpdateItemAsync(Guid itemId, ItemModel model, CancellationToken ct = default)
+        Task<List<ItemDto>> IItemService.GetItemsAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ItemDto> IItemService.UpdateItemAsync(Guid itemId, ItemModel model, CancellationToken ct)
         {
             throw new NotImplementedException();
         }
